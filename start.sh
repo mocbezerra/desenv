61a71a5a440e91f86eb5564be1327e38233cb7a1
Mateus Bezerra <mocbezerra@gmail.com>
2019-02-20 06:02:18 +0000

    fazendo reajustes

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 apt-get update -y
 apt-get upgrade -y
 
-apt-get install git vim -y
+# Dependências
+apt-get install git -y
+apt-get install vim -y
 
+# Configurações GIT
 git config --global user.name "Mateus Bezerra"
 git config --global user.email "mocbezerra@gmail.com"
